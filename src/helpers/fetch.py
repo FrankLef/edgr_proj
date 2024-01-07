5b04b8a5090cfeaabfbeb34699edd53c68287a8d
FrankLef <flefebvre01@hotmail.com>
2024-01-07 17:10:51 -0500

    fix: docstrings

--- src/helpers/fetch.py
+++ src/helpers/fetch.py
@@ -9,11 +9,27 @@ import src.helpers.write as write
 
 def fetch_rss(
     url: str,
-    path: Path = Path.cwd(),
+    path: Path,
     period: datetime = datetime.now(),
     head: bool = False,
     overwrite: bool = False,
 ) -> int:
+    """Download the rss feed providing the specs to retrieve the xbrl files.
+
+    Args:
+        url (str): The SEC's url for tthr rss feed.
+        path (Path): Path where the downloaded rss feed will be stored..
+        period (datetime, optional): Date with the year and month used to fetch the rss and name the download file. Defaults to datetime.now().
+        head (bool, optional): True=Do a http HEAD request method. Defaults to False.
+        overwrite (bool, optional): Overwrite existing downloaded file. Defaults to False.
+
+    Raises:
+        FileNotFoundError: The `path` does not exist.
+        FileExistsError: The file already exist and is not allowed to be overwritten by `overwrite`.
+
+    Returns:
+        int: The status code for the url request.
+    """
     # create the file name and add it to the path
     if path.exists():
         fn = write.write_fn(period=period)
